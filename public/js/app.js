a41c86e0ddf6515c6fb286d12618b8d6b174a891
Smriti Singh <smriti2815@gmail.com>
2021-05-17 22:09:50 +0530

    Some Changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,5 @@
 console.log("client side javascript")
 
-// fetch('http://puzzle.mead.io/puzzle').then((response) => {
-//     response.json().then((data) => {
-//         console.log(data);
-//     })
-// })
-
 const userInput = document.querySelector('form');
 const search = document.querySelector('input');
 const paragraphOne = document.querySelector('#paragraph-1');
