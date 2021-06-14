f5b6bb33c949845b8212aa39f11fa262a761c1ad
Smriti Singh <smriti2815@gmail.com>
2021-06-14 14:23:21 +0530

    Add api keys

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -2,9 +2,10 @@ const request = require('postman-request');
 
 
 const forecast = (latitude,longitude,callback) =>{
-    const url = 'http://api.weatherstack.com/current?access_key=741073be24ac0ef351523c63ed7097e4&query='+longitude+','+latitude+'&units=f';
+    const url = 'http://api.weatherstack.com/current?access_key='+process.env.WEATHER_STACK_ACCESS_KEY+'&query='+longitude+','+latitude+'&units=f';
 
     request({url,json:true},(error,{body})=>{
+        console.log(body);
         if(error) {
             callback('Cannot Connect to the weather Services',undefined);
         } else if(body.error) {
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require('postman-request');
 
 const geocode = (address,callback) => {
-    const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token=pk.eyJ1Ijoic2FtMjgxNSIsImEiOiJja29lOTA4ankwNmJqMm5wN3ZieWQxc2RnIn0.dhT0QkIfX0TeKqKnfpr6Dg&limit=1';
+    const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/'+encodeURIComponent(address)+'.json?access_token='+process.env.MAP_BOX_ACCESS_TOKEN+'&limit=1';
 
     request({url:url,json:true},(error,{body})=>{
         if(error){
